c137d2df3c2968f76be65dfb42af87d6c1c889c8
VinuthnaGummadi <vinuthna.gummadi@gmail.com>
2018-06-02 15:32:10 -0400

    feat(module-api): update TruckerApplication.java to add Async functionality

--- src/main/java/io/egen/trucker/TruckerApplication.java
+++ src/main/java/io/egen/trucker/TruckerApplication.java
@@ -1,12 +1,37 @@
 package io.egen.trucker;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
-public class TruckerApplication {
+@EnableAsync
+public class TruckerApplication extends AsyncConfigurerSupport {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TruckerApplication.class, args);
+	}
+
+	@Override
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(7);
+		executor.setMaxPoolSize(50);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("Trucker Thread");
+		executor.initialize();
+		return executor;
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		return KieServices.Factory.get().getKieClasspathContainer();
 	}
 }
