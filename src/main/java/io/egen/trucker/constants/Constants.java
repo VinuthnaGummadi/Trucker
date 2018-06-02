dffba5bb2922364e6fd17e62109dd72e0b5d3e17
VinuthnaGummadi <vinuthna.gummadi@gmail.com>
2018-06-02 15:25:01 -0400

    feat(module-api): add Constants file

---
+++ src/main/java/io/egen/trucker/constants/Constants.java
@@ -0,0 +1,13 @@
+package io.egen.trucker.constants;
+
+public class Constants {
+    public static final String ALERT_MEDIUM = "MEDIUM";
+
+    public static final String ALERT_HIGH = "HIGH";
+
+    public static final String ALERT_LOW = "LOW";
+
+    public static final String ASC="ASC";
+
+    public static final String DESC="DESC";
+}
